a9b592c50074503bc82b687272190c0d8ccfdf0a
Alvaro <al7aro@gmail.com>
2022-09-19 12:11:11 +0200

    Basic Reader
    Unit Tests NOT added yet
    	All the testing was made directly from MAIN

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: yoav <yoav@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 09:50:39 by al7aro            #+#    #+#             */
-/*   Updated: 2022/09/18 04:40:35 by alopez-g         ###   ########.fr       */
+/*   Updated: 2022/09/19 12:10:07 by al7aro-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,9 @@
 int	main(int argc, char **argv)
 {
 	t_shell_op		op;
-	t_error_code	err;
 
+	(void)argc;
+	(void)argv;
 	printf("~Minishell~\n\n");
-	arg_reader(argc, argv);
-	while (1)
-	{
-		err = reader(&op.input);
-		info_log_input_table(op.input);
-	}
 	return (0);
 }
