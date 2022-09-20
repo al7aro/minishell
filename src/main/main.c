5eda3e0a8ab5d52df3a70c405da86639eabb4564
Alvaro <al7aro@gmail.com>
2022-09-20 10:50:55 +0200

    removed useless comments

--- src/main/main.c
+++ src/main/main.c
@@ -6,15 +6,12 @@
 /*   By: yoav <yoav@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 09:50:39 by al7aro            #+#    #+#             */
-/*   Updated: 2022/09/19 18:57:26 by al7aro-g         ###   ########.fr       */
+/*   Updated: 2022/09/20 10:49:25 by al7aro-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-/*
- * A PROMPT string should be together with global vars
- * */
 int	main(int argc, char **argv)
 {
 	(void)argc;
