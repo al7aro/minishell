7e0222ae1af0bdf3d5eb8ed352590554c8174f4c
Alvaro <al7aro@gmail.com>
2022-09-20 14:45:04 +0200

    leak

--- unit_test/reader/reader.t.c
+++ unit_test/reader/reader.t.c
@@ -6,13 +6,19 @@
 /*   By: yoav <yoav@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 09:49:12 by yoav              #+#    #+#             */
-/*   Updated: 2022/09/20 14:42:54 by al7aro           ###   ########.fr       */
+/*   Updated: 2022/09/20 14:44:45 by al7aro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "unit_test.h"
+#include <stdlib.h>
 
 void	test(void)
 {
+	char	*leak;
+
+	leak = malloc(100);
+	leak = 0;
+	(void)leak;
 	CU_ASSERT(CU_TRUE);
 }
