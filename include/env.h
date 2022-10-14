4f73e9015c29fe6601ff71821d713b712172f12c
Alvaro <al7aro@gmail.com>
2022-10-14 13:27:13 +0200

    env

--- include/env.h
+++ include/env.h
@@ -5,25 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: al7aro <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/08 19:54:24 by al7aro            #+#    #+#             */
-/*   Updated: 2022/10/08 20:57:25 by al7aro           ###   ########.fr       */
+/*   Created: 2022/10/12 14:00:21 by al7aro            #+#    #+#             */
+/*   Updated: 2022/10/12 14:00:24 by al7aro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
 
-#include "dll.h"
-#include "error_code.h"
-#include "libft.h"
-
-typedef struct	s_env
-{
-	char	*key;
-	char	*value;
-}				t_env;
-
-t_env	*env_create(char *key, char *value);
-void	env_destroy(t_env **env);
+char	*env_getenv();
 
 #endif
