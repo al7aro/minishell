855c8f3406f703155ad256af734a4f117ef7e3e5
alopez-g <alopez-g@c2r8s3.42madrid.com>
2022-10-15 21:52:15 +0200

    merge and fix

--- include/env_utils.h
+++ include/env_utils.h
@@ -6,7 +6,7 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 21:15:06 by alopez-g          #+#    #+#             */
-/*   Updated: 2022/10/15 21:18:15 by alopez-g         ###   ########.fr       */
+/*   Updated: 2022/10/15 21:50:57 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 # define ENV_UTILS_H
 
 # include "error_code.h"
-# include "cmd.h"
+# include "shell_op.h"
 
 char			*get_key_value_pair(char *key, char *value);
-t_error_code	new_var(t_cmd *sp, char *key, char *value);
+t_error_code	new_var(t_shell_op *sp, char *key, char *value);
 t_bool			key_compare(char *env, char *key);
 char			*get_value(char *env_var);
 int				env_size(char **envp);
