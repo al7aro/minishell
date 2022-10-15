9cec84f8929663e2ec67d32c07e0d70885dc988a
alopez-g <alopez-g@c2r8s3.42madrid.com>
2022-10-16 00:39:28 +0200

    include fix

--- include/env_utils.h
+++ include/env_utils.h
@@ -6,14 +6,14 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 21:15:06 by alopez-g          #+#    #+#             */
-/*   Updated: 2022/10/15 22:19:55 by alopez-g         ###   ########.fr       */
+/*   Updated: 2022/10/16 00:38:19 by alopez-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_UTILS_H
 # define ENV_UTILS_H
 
-# include "shell_op.h"
+# include "macro.h"
 
 char			*get_key_value_pair(char *key, char *value);
 t_error_code	new_var(char ***env, char *key, char *value);
