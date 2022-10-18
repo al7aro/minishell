fbcfc9c3e434d4002653cc94f9141b4f1e4cdd60
Alvaro <al7aro@gmail.com>
2022-10-18 22:12:04 +0200

    char remover init

---
+++ src/expander/expander2.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expander2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: al7aro <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 22:07:07 by al7aro            #+#    #+#             */
+/*   Updated: 2022/10/18 22:07:07 by al7aro           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "expander.h"
+
+/*
+ * static char	*remove_char_from_str(char *str, char *pos)
+ * {
+ * 	(void)str;
+ * 	(void)pos;
+ * 	return (NULL);
+ * }
+*/
+
+char	*expander_remove_quotes(char *str)
+{
+	char	*ret;
+
+	(void)str;
+	ret = NULL;
+	return (ret);
+}
