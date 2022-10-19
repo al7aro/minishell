f1c99c0dd4705a49e828f74d1d0ae9ba9b966093
Alvaro <al7aro@gmail.com>
2022-10-19 10:10:18 +0200

    char remover | TODO: quote pair remover

--- src/expander/expander2.c
+++ src/expander/expander2.c
@@ -12,20 +12,35 @@
 
 #include "expander.h"
 
-/*
- * static char	*remove_char_from_str(char *str, char *pos)
- * {
- * 	(void)str;
- * 	(void)pos;
- * 	return (NULL);
- * }
-*/
+
+static char	*remove_pair_from_str(char *str, char *c1, char *c2)
+{
+	size_t	len;
+	size_t	i_ret;
+	char	*ret;
+	char	*tmp;
+
+	tmp = str;
+	len = ft_strlen(str) - 1;
+	ret = (char *)malloc(sizeof(char) * len);
+	*(ret + len) = 0;
+	i_ret = 0;
+	while (*(str))
+		if (str++ !=  c1 && str - 1 != c2)
+			*(ret + i_ret++) = *(str - 1);
+	free(tmp);
+	return (ret);
+}
+
 
 char	*expander_remove_quotes(char *str)
 {
 	char	*ret;
 
 	(void)str;
-	ret = NULL;
+	ret = ft_strdup("Alv-(aro");
+	printf("%s\n", ret);
+	ret = remove_pair_from_str(ret, ret + 3, ret + 4);
+	printf("%s\n", ret);
 	return (ret);
 }
