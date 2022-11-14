7e28649d5187de126b1d8994a17d2b5100e6616e
Alvaro <al7aro@gmail.com>
2022-11-14 13:06:56 +0100

    initcommit

---
+++ include/heredoc.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: al7aro <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 12:23:44 by al7aro            #+#    #+#             */
+/*   Updated: 2022/11/14 12:26:41 by al7aro           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEREDOC_H
+# define HEREDOC_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
