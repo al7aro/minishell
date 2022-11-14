beaa5c756801ccfb78a6df6d0d20e4f3a1a2c35e
Alvaro <al7aro@gmail.com>
2022-11-14 13:38:20 +0100

    norm

--- include/heredoc.h
+++ include/heredoc.h
@@ -6,7 +6,7 @@
 /*   By: al7aro <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 12:23:44 by al7aro            #+#    #+#             */
-/*   Updated: 2022/11/14 13:26:22 by al7aro           ###   ########.fr       */
+/*   Updated: 2022/11/14 13:38:12 by al7aro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-#include "error_code.h"
+# include "error_code.h"
 
 t_error_code	heredoc_get_line(char **line, char *eol);
 
