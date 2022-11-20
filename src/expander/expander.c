ddd4ace2155d05f73c5c5c3755e90b35bfd74215
al7aro <al7aro@gmail.com>
2022-11-20 17:17:03 +0100

    fix

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,11 +6,12 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 10:05:08 by alopez-g          #+#    #+#             */
-/*   Updated: 2022/11/14 13:13:36 by al7aro-g         ###   ########.fr       */
+/*   Updated: 2022/11/20 17:16:30 by r3dc4t-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expander.h"
+#include "macro.h"
 #include "reader.h"
 #include "tab.h"
 
@@ -91,21 +92,29 @@ static int	expander_get_var(t_shell_op *sp, char *str, char **ret)
 	return (0);
 }
 
+static	t_bool	inside_quote(t_bool flag, char c)
+{
+	if (c == '\"')
+		return (!flag);
+	return (flag);
+}
+
 char	*expander_expand_var(t_shell_op *sp, char *str)
 {
 	char	*ret;
 	char	*exp;
 	char	*tmp;
 	int		i;
+	t_bool	expand;
 
-	i = 0;
-	if (!(sp->envp))
-		return (ft_strdup(str));
+	expand = TRUE;
+	i = -1;
 	ret = ft_strdup("");
-	while (*(str + i))
+	while (*(str + ++i) && (sp->envp))
 	{
+		expand = inside_quote(expand, *(str + i));
 		tmp = ret;
-		if (EXPANDER_CHAR == *(str + i))
+		if (EXPANDER_CHAR == *(str + i) && expand)
 		{
 			i += expander_get_var(sp, str + i, &exp);
 			exp = word_encloser(exp);
@@ -115,7 +124,6 @@ char	*expander_expand_var(t_shell_op *sp, char *str)
 		}
 		else
 			ret = str_append_char(&ret, *(str + i), 1);
-		i++;
 	}
 	return (ret);
 }
